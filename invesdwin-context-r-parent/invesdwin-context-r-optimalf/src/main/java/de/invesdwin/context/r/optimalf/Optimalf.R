0c28436412b2e7d60c2dce7c3770df66eee4fbde
subes <gsubes@gmail.com>
2017-04-30 00:16:51 +0200

    package name fix

--- invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/Optimalf.R
+++ invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/Optimalf.R
@@ -1,4 +1,4 @@
-library("de.invesdwin.context.r.renjin.packages.lspm");
+library("de.invesdwin:invesdwin-context-r-renjin-packages-lspm");
 trades <- c(2,-1);
 probs <- c(0.5,0.5);
 lspobj <- lsp(trades,probs);
