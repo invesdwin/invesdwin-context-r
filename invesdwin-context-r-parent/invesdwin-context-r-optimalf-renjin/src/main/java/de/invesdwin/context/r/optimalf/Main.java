2a8d4bbb6791a988e19a4e468ec4e07b28f1c56c
subes <gsubes@gmail.com>
2017-05-01 18:47:49 +0200

    jri alternative

---
+++ invesdwin-context-r-parent/invesdwin-context-r-optimalf-renjin/src/main/java/de/invesdwin/context/r/optimalf/Main.java
@@ -0,0 +1,30 @@
+package de.invesdwin.context.r.optimalf;
+
+import javax.annotation.concurrent.Immutable;
+
+import org.kohsuke.args4j.CmdLineParser;
+
+import de.invesdwin.context.PlatformInitializerProperties;
+import de.invesdwin.context.beans.init.AMain;
+
+@Immutable
+public class Main extends AMain {
+
+    static {
+        PlatformInitializerProperties.setAllowed(false);
+    }
+
+    protected Main(final String[] args) {
+        super(args, false);
+    }
+
+    public static void main(final String[] args) {
+        new Main(args);
+    }
+
+    @Override
+    protected void startApplication(final CmdLineParser parser) throws Exception {
+        new Optimalf(null).getOptimalfPerStrategy();
+    }
+
+}
