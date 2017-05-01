b39e284398d47ff10d501e8ec264dd802167e03a
subes <gsubes@gmail.com>
2017-05-01 12:30:58 +0200

    JRI module

--- invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/Optimalf.R
+++ invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/Optimalf.R
@@ -1,10 +1,10 @@
-#library("de.invesdwin.context.r.renjin.packages:LSPM");
-trades <- c(2,-1)
-probs <- c(0.5,0.5)
-lspobj <- lsp(trades,probs)
-lspobj
-result <- optimalf(lspobj)
-result
-lspobj$f <- result$f  
-lspobj
+library("de.invesdwin.context.r.renjin.packages:LSPM");
+trades <- c(2,-1);
+probs <- c(0.5,0.5);
+lspobj <- lsp(trades,probs);
+lspobj;
+result <- optimalf(lspobj);
+result;
+lspobj$f <- result$f;  
+lspobj;
 
