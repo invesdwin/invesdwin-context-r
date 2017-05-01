008193355764618305d982bca11a9d7b804aff3c
subes <gsubes@gmail.com>
2017-05-01 20:42:44 +0200

    compile fix

--- invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/Optimalf.java
+++ invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/Optimalf.java
@@ -1,14 +1,10 @@
 package de.invesdwin.context.r.optimalf;
 
-import java.io.InputStreamReader;
 import java.util.List;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
-import io.onetapbeyond.renjin.r.executor.Renjin;
 import io.onetapbeyond.renjin.r.executor.RenjinException;
-import io.onetapbeyond.renjin.r.executor.RenjinResult;
-import io.onetapbeyond.renjin.r.executor.RenjinTask;
 
 @NotThreadSafe
 public class Optimalf {
@@ -20,13 +16,13 @@ public class Optimalf {
     }
 
     public List<Double> getOptimalfPerStrategy() throws RenjinException {
-        final RenjinTask task = Renjin.R(true, true)
-                .code(new InputStreamReader(getClass().getResourceAsStream("Optimalf.R")))
-                .build();
-        final RenjinResult result = task.execute();
-        if (!result.success()) {
-            throw new IllegalStateException("Error: " + result.error(), result.cause());
-        }
+        //        final RenjinTask task = Renjin.R(true, true)
+        //                .code(new InputStreamReader(getClass().getResourceAsStream("Optimalf.R")))
+        //                .build();
+        //        final RenjinResult result = task.execute();
+        //        if (!result.success()) {
+        //            throw new IllegalStateException("Error: " + result.error(), result.cause());
+        //        }
         return null;
     }
 
