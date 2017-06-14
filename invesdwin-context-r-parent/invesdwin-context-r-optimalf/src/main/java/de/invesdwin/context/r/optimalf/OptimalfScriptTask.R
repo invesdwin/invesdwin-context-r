d41b04d566096c00437bc78ef900d8ca0d130533
subes <gsubes@gmail.com>
2017-06-14 13:15:44 +0200

    example

--- invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/OptimalfScriptTask.R
+++ invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/OptimalfScriptTask.R
@@ -1,6 +1,6 @@
 library(LSPM)
 library(DEoptim)
-trades <- c(1, .15, .2, -.1)
+trades <- c(0.1, -.15, .2, -.1)
 #trades <- c(2, -3, 10, -5)
 probs <- c(0.25,0.25,0.25,0.25)
 lspobj <- lsp(trades,probs)
