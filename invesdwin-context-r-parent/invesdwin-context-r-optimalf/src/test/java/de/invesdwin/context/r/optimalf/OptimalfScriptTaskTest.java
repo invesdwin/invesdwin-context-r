d27df5f502689217effa602a66e34acdc4277b28
subes <subes@subes-gaming.fritz.box>
2017-12-24 21:27:20 +0100

    remove renjin test

--- invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/test/java/de/invesdwin/context/r/optimalf/OptimalfScriptTaskTest.java
+++ invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/test/java/de/invesdwin/context/r/optimalf/OptimalfScriptTaskTest.java
@@ -7,13 +7,11 @@ import java.util.List;
 import javax.annotation.concurrent.NotThreadSafe;
 import javax.inject.Inject;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import de.invesdwin.context.r.runtime.contract.IScriptTaskRunnerR;
 import de.invesdwin.context.r.runtime.jri.JriScriptTaskRunnerR;
 import de.invesdwin.context.r.runtime.rcaller.RCallerScriptTaskRunnerR;
-import de.invesdwin.context.r.runtime.renjin.RenjinScriptTaskRunnerR;
 import de.invesdwin.context.r.runtime.rserve.RserveScriptTaskRunnerR;
 import de.invesdwin.context.test.ATest;
 import de.invesdwin.util.assertions.Assertions;
@@ -29,8 +27,6 @@ public class OptimalfScriptTaskTest extends ATest {
     private RserveScriptTaskRunnerR rserveScriptTaskRunner;
     @Inject
     private JriScriptTaskRunnerR jriScriptTaskRunner;
-    @Inject
-    private RenjinScriptTaskRunnerR renjinScriptTaskRunner;
 
     @Test
     public void testRCaller() {
@@ -66,15 +62,6 @@ public class OptimalfScriptTaskTest extends ATest {
     public void testJri() {
         for (int i = 0; i < ITERATIONS; i++) {
             run(jriScriptTaskRunner);
-            log.info("------------------------");
-        }
-    }
-
-    @Ignore("org.renjin.eval.EvalException: objective function result has different length than parameter matrix")
-    @Test
-    public void testRenjin() {
-        for (int i = 0; i < ITERATIONS; i++) {
-            run(renjinScriptTaskRunner);
             log.info("------------------------");
         }
     }
