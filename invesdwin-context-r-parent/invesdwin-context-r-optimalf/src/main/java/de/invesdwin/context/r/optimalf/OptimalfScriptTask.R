447d6f16401b28c7f22aacdaffcfafd867bc0af3
subes <gsubes@gmail.com>
2018-08-27 13:50:57 +0200

    install packages if missing

--- invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/OptimalfScriptTask.R
+++ invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/main/java/de/invesdwin/context/r/optimalf/OptimalfScriptTask.R
@@ -1,6 +1,23 @@
-library(LSPM)
-library(DEoptim)
-#library(snow)
+#sudo apt-get -y build-dep libcurl4-gnutls-dev
+#sudo apt-get -y install libcurl4-gnutls-dev
+if (!require("LSPM")) {
+  if (!require("devtools")) {
+    install.packages("devtools")
+    library("devtools")
+  }
+  install_github("joshuaulrich/lspm")
+  library("LSPM")
+}
+
+if (!require("DEoptim")) {
+  install.packages("DEoptim")
+  library("DEoptim")
+}
+
+#if (!require("snow")) {
+#  install.packages("snow")
+#  library("snow")
+#}
 
 #clust <- makeSOCKcluster(2)
 clust <- NULL
@@ -9,21 +26,21 @@ clust <- NULL
 #trades <- c(2, -3, 10, -5)
 #probabilities <- cbind(c(0.25,0.25,0.25,0.25), c(0.25,0.25,0.25,0.25))
 
-if(length(trades) == 0){
-	stop("No trades!")
+if (length(trades) == 0) {
+  stop("No trades!")
 }
 
-portfolio <- lsp(trades,probabilities)
+portfolio <- lsp(trades, probabilities)
 
-DEctrl <- list(NP=30,itermax=200)
-result <- optimalf(portfolio, snow=clust, control=DEctrl)
+DEctrl <- list(NP = 30, itermax = 200)
+result <- optimalf(portfolio, snow = clust, control = DEctrl)
 #result <- optimalf(portfolio, probDrawdown, 0.1, DD=0.2, horizon=4, snow=clust, control=DEctrl)
 #result <- optimalf(portfolio, probRuin, 0.1, DD=0.2, horizon=4, snow=clust, control=DEctrl)
-profit <- result$G-1
-if(profit == -Inf){
-	profit <- -999
-}else if(profit == Inf){
-	profit <- 999
+profit <- result$G - 1
+if (profit == -Inf) {
+  profit <- -999
+} else if (profit == Inf) {
+  profit <- 999
 }
-	
+
 optimalf <- result$f
--- invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/test/java/de/invesdwin/context/r/optimalf/OptimalfTests.R
+++ invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/test/java/de/invesdwin/context/r/optimalf/OptimalfTests.R
@@ -1,19 +1,40 @@
-library(LSPM)
-library(DEoptim)
-library(matrixStats)
-library(snow)
+#sudo apt-get -y build-dep libcurl4-gnutls-dev
+#sudo apt-get -y install libcurl4-gnutls-dev
+if (!require("LSPM")) {
+  if (!require("devtools")) {
+    install.packages("devtools")
+    library("devtools")
+  }
+  install_github("joshuaulrich/lspm")
+  library("LSPM")
+}
+
+if (!require("DEoptim")) {
+  install.packages("DEoptim")
+  library("DEoptim")
+}
+
+if (!require("matrixStats")) {
+  install.packages("matrixStats")
+  library("matrixStats")
+}
+
+if (!require("snow")) {
+  install.packages("snow")
+  library("snow")
+}
 
 #clust <- makeSOCKcluster(2)
 clust <- NULL
 
-trades <- cbind(c(.5, -.3, .4, -.2),c(0.1, -.15, .4, -.1))
-probs <- cbind(c(0.25,0.25,0.25,0.25), c(0.25,0.25,0.25,0.25))
+trades <- cbind(c(.5,-.3, .4,-.2), c(0.1,-.15, .4,-.1))
+probs <- cbind(c(0.25, 0.25, 0.25, 0.25), c(0.25, 0.25, 0.25, 0.25))
 #trades <- c(2, -3, 10, -5)
 #probs <- c(0.25,0.25,0.25,0.25)
-lspobj <- lsp(trades,probs)
+lspobj <- lsp(trades, probs)
 
-DEctrl <- list(NP=30,itermax=200)
-result <- optimalf(lspobj, snow=clust, control=DEctrl)
+DEctrl <- list(NP = 30, itermax = 200)
+result <- optimalf(lspobj, snow = clust, control = DEctrl)
 #result <- optimalf(lspobj, probDrawdown, 0.1, DD=0.2, horizon=4, snow=clust, control=DEctrl)
 #result <- optimalf(lspobj, probRuin, 0.1, DD=0.2, horizon=4, snow=clust, control=DEctrl)
 profit <- result$G
