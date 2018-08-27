ba5b314fa14929a33bf7fc13966c452d2a62ae7c
subes <gsubes@gmail.com>
2018-08-27 14:31:44 +0200

    testsuite

---
+++ invesdwin-context-r-parent/invesdwin-context-r-optimalf/src/test/java/de/invesdwin/context/r/optimalf/OptimalfTestSuite.java
@@ -0,0 +1,14 @@
+package de.invesdwin.context.r.optimalf;
+
+import javax.annotation.concurrent.Immutable;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ OptimalfScriptTaskTest.class })
+@Immutable
+public class OptimalfTestSuite {
+
+}
