5d8d2e7e3780eac043354430c26adfe24b77c48f
subes <gsubes@gmail.com>
2019-02-22 12:56:24 +0100

    use absolute path to sink file

--- invesdwin-context-r-parent/invesdwin-context-r-runtime-rserve/src/main/java/de/invesdwin/context/r/runtime/rserve/pool/internal/RsessionPoolableObjectFactory.java
+++ invesdwin-context-r-parent/invesdwin-context-r-runtime-rserve/src/main/java/de/invesdwin/context/r/runtime/rserve/pool/internal/RsessionPoolableObjectFactory.java
@@ -12,6 +12,7 @@ import org.math.R.Rsession;
 import org.math.R.StartRserve;
 import org.springframework.beans.factory.FactoryBean;
 
+import de.invesdwin.context.ContextProperties;
 import de.invesdwin.context.pool.IPoolableObjectFactory;
 import de.invesdwin.context.r.runtime.contract.IScriptTaskRunnerR;
 import de.invesdwin.context.r.runtime.rserve.RserveProperties;
@@ -20,8 +21,8 @@ import de.invesdwin.context.r.runtime.rserve.RserverConfMode;
 import de.invesdwin.context.system.OperatingSystem;
 import de.invesdwin.util.error.UnknownArgumentException;
 import de.invesdwin.util.lang.Reflections;
+import de.invesdwin.util.lang.UniqueNameGenerator;
 import de.invesdwin.util.time.fdate.FDate;
-import de.invesdwin.util.time.fdate.FTimeUnit;
 
 @ThreadSafe
 @Named
@@ -29,6 +30,7 @@ public final class RsessionPoolableObjectFactory
         implements IPoolableObjectFactory<Rsession>, FactoryBean<RsessionPoolableObjectFactory> {
 
     public static final RsessionPoolableObjectFactory INSTANCE = new RsessionPoolableObjectFactory();
+    private static final UniqueNameGenerator UNIQUE_NAME_GENERATOR = new UniqueNameGenerator();
 
     @GuardedBy("this.class")
     private static FDate lastTimestamp = FDate.MIN_DATE;
@@ -41,26 +43,11 @@ public final class RsessionPoolableObjectFactory
     public Rsession makeObject() {
         maybeInitialize();
         final Rsession session = createSession();
-        final FDate timestamp = newTimestamp();
-        Reflections.field("SINK_FILE")
-                .ofType(String.class)
-                .in(session)
-                .set(".Rout-" + timestamp.toString(FDate.FORMAT_TIMESTAMP_UNDERSCORE));
+        //use absolute path or else connection might not work properly
+        final String sinkFile = new File(ContextProperties.TEMP_DIRECTORY, UNIQUE_NAME_GENERATOR.get(".Rout"))
+                .getAbsolutePath();
+        Reflections.field("SINK_FILE").ofType(String.class).in(session).set(sinkFile);
         return session;
-    }
-
-    private static synchronized FDate newTimestamp() {
-        FDate timestamp = new FDate();
-        while (timestamp.equals(lastTimestamp)) {
-            try {
-                FTimeUnit.MILLISECONDS.sleep(1);
-            } catch (final InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            timestamp = new FDate();
-        }
-        lastTimestamp = timestamp;
-        return timestamp;
     }
 
     private Rsession createSession() {
